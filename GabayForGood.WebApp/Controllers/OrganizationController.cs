1bfc87f32cec00003d721e7bc9fd55a788d3d54e
[Lei] <[leiivan.villacorta@benilde.edu.ph]>
2025-08-09 23:27:39 +0800

    projects in progress

--- GabayForGood.WebApp/Controllers/OrganizationController.cs
+++ GabayForGood.WebApp/Controllers/OrganizationController.cs
@@ -1,23 +1,33 @@
-﻿using GabayForGood.DataModel;
+﻿using AutoMapper;
+using GabayForGood.DataModel;
+using GabayForGood.WebApp.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GabayForGood.WebApp.Controllers
 {
     public class OrganizationController : Controller
     {
+        private readonly SignInManager<ApplicationUser> signInManager;
         private readonly UserManager<ApplicationUser> userManager;
+        private readonly IMapper mapper;
+        private readonly AppDbContext context;
 
-        public OrganizationController(UserManager<ApplicationUser> userManager)
+        public OrganizationController(AppDbContext context, IMapper mapper, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
+            this.context = context;
+            this.mapper = mapper;
+            this.signInManager = signInManager;
             this.userManager = userManager;
         }
 
         [Authorize(Roles = "Organization")]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var projs = await context.Projects.ToListAsync();
+            return View(mapper.Map<List<OrgVM>>(projs));
         }
     }
 }
